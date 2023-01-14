b1a9828be55052d452a25a2e703d46ff55937ce4
mitisit <mitisit89@gmail.com>
2023-01-14 14:14:21 +0300

    update deps

--- clientsviewer/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from celery import Celery 
-
-app=Celery('clientsviewer')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clientsviewer.settings')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
