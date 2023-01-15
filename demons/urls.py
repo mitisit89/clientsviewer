140ae9d08087975f47c4803d4ff6cdb1c8dc184f
mitisit <mitisit89@gmail.com>
2023-01-15 15:49:21 +0300

    Improve views

--- demons/urls.py
+++ demons/urls.py
@@ -1,10 +1,5 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+
 from .views import get_memory_left
 
-
-urlpatterns=[
-        path('check_memory',get_memory_left)
-        ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = [path("check_memory", get_memory_left)]
--- demons/views.py
+++ demons/views.py
@@ -4,19 +4,17 @@ from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from .serializers import MemoryCheckerSerializer
 from clientsviewer.settings import BASE_DIR
+
+from .serializers import MemoryCheckerSerializer
+
+
 @api_view(["GET"])
 def get_memory_left(request: Request) -> Response:
     """
-    Assept get Request,returns json array with date and amount of memory available on the host  
+    Assept get request,returns json array with date and amount of memory available on the host
     """
-    try:
-        memory_logs = open(f'{BASE_DIR}/logs.json','r',encoding='utf-8')
-        f=json.load(memory_logs)
-        result = MemoryCheckerSerializer(f)
+    with open(f"{BASE_DIR}/logs.json", "r", encoding="utf-8") as f:
+        memory_log = json.load(f)
+        result = MemoryCheckerSerializer(memory_log)
         return Response(result.data)
-    except ValueError as e:
-        pass
-    finally:
-        memory_logs.close()
