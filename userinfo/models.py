1ea3815fe249c675f62d305c63f511bac57a8e76
mitisit <mitisit89@gmail.com>
2023-01-16 11:49:37 +0300

    Update models

--- userinfo/models.py
+++ userinfo/models.py
@@ -1,8 +1,37 @@
 import uuid
+from typing import Optional
 
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 from django.db import models
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your models here.
+
+class UserModelManager(BaseUserManager):
+    def create_user(
+        self,
+        name: str,
+        email: str,
+        password: Optional[str] = None,
+    ) -> "UserModel":
+        if name is None:
+            raise TypeError("Users must have a username.")
+        if email is None:
+            raise TypeError("Users must have an email address")
+        user = self.create_user(name, email, password)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, name: str, email: str, password: str) -> "UserModel":
+        if password is None:
+            raise TypeError("Superusers must have a password.")
+        user = self.create_user(name, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_active = True
+        user.save()
+        return user
 
 
 class UserPhoto(models.Model):
@@ -10,10 +39,25 @@ class UserPhoto(models.Model):
     photo = models.ImageField(upload_to="images/")
 
 
-class UserModel(models.Model):
+class UserModel(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     name = models.CharField(max_length=150, db_index=True)
     surname = models.CharField(max_length=150, db_index=True)
     sex = models.CharField(max_length=10, db_index=True)
     birthday = models.DateTimeField()
     user_photo = models.ForeignKey(UserPhoto, on_delete=models.CASCADE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+    objects = UserModelManager()
+
+    @property
+    def tokens(self) -> dict[str, str]:
+        refresh_token = RefreshToken.for_user(self)
+        return {
+            "refresh": str(refresh_token),
+            "access": str(refresh_token.access_token),
+        }
