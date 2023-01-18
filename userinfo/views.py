a87386779576e6270e0f0ff1dad8a95ab0b6a8ca
mitisit <mitisit89@gmail.com>
2023-01-18 20:25:04 +0300

    work in progress

--- userinfo/views.py
+++ userinfo/views.py
@@ -8,29 +8,30 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .renderers import UserJSONRenderer
-from .serializers import (LoginUserModelSerializer, LogoutSerializer,
-                          RegistrationUserModelSerializer, UserModelSerializer)
+from .serializers import (LoginUserSerializer, LogoutSerializer,
+                          RegistrationUserSerializer, UserSerializer)
 
 
 class RegistrationApi(APIView):
     permission_classes = (AllowAny,)
-    renderer_classes = (UserJSONRenderer,)
-    serializer_class = RegistrationUserModelSerializer
-    parser_classes=(parsers.MultiPartParser,)
+    renderer_classes= (UserJSONRenderer,)
+    serializer_class = RegistrationUserSerializer
+    parser_classes=(parsers.MultiPartParser,parsers.FileUploadParser)
 
     def post(self, request: Request) -> Response:
         """Return user response after a successful registration."""
         user_request = request.data
         serializer = self.serializer_class(data=user_request)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class LoginApi(APIView):
     permission_classes = (AllowAny,)
     renderer_classes = (UserJSONRenderer,)
-    serializer_class = LoginUserModelSerializer
+    serializer_class = LoginUserSerializer
 
     def post(self, request: Request) -> Response:
         """Return user after login."""
@@ -44,7 +45,7 @@ class LoginApi(APIView):
 class UserRetrieveUpdateApi(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated,)
     renderer_classes = (UserJSONRenderer,)
-    serializer_class = UserModelSerializer
+    serializer_class = UserSerializer
     lookup_url_kwarg = "id"
     parser_classes = [
         parsers.MultiPartParser,
@@ -61,12 +62,12 @@ class UserRetrieveUpdateApi(RetrieveUpdateDestroyAPIView):
         self, request: Request, *args: type(Any), **kwargs: dict[str, Any]
     ) -> Response:
         serializer_data = request.data.get("user", {})
-        serializer = UserModelSerializer(
+        serializer = UserSerializer(
             request.user, data=serializer_data, partial=True
         )
         if serializer.is_valid():
             user = serializer.save()
-            return Response(UserModelSerializer(user).data)
+            return Response(UserSerializer(user).data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
